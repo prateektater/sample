8d94adc2b7131187a26f300819bc62433ad6e26d
prateektater@gmail.com <prateektater@gmail.com>
2018-12-15 12:09:20 -0500

    adding 404 error and detail.html

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import Http404
+
 #from django.template import loader
 # Create your views here.
 from django.http import HttpResponse
@@ -17,7 +19,12 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    #return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question.id})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
