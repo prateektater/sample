84a6d427c8c829874dec28e8473f8ddba13c63d6
prateektater@gmail.com <prateektater@gmail.com>
2018-12-17 04:09:50 -0500

    changing and customizing admin view

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+#admin.site.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
+#class ChoiceInline(admin.StackedInline):
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
+#admin.site.register(Choice)
